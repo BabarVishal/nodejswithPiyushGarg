0199fe1b580cc7b0e97aaa2c0b84ad520eea5560
vishal <vishalbabar7351@gmail.com>
2024-07-29 23:41:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const {add, sub} = require("./math.js")
+
+const Add = add(2,2);
+const Sub = sub(6,2);
+console.log("THe Value Of add Function Is :",Add);
+console.log("The Value Of Sub Function Is :",Sub);
+
+
+
